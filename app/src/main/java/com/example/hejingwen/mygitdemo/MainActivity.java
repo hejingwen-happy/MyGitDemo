4c2b5c6100b4cc6a4d450e98af32ff80204d26a2
HeJingWen <271703614@qq.com>
2018-07-07 11:19:20 +0800

    改了注释

--- app/src/main/java/com/example/hejingwen/mygitdemo/MainActivity.java
+++ app/src/main/java/com/example/hejingwen/mygitdemo/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //我要是测试
         setContentView(R.layout.activity_main);
     }
 }
